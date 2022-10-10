359e00b691ef050ddb3f6b1ea94733a238506888
mszucseducation <113265275+mszucseducation@users.noreply.github.com>
2022-10-09 18:10:18 -0700

    new

---
+++ Week5/V3-Code-login-popup-web-change-pages/pages/Login/index.js
@@ -0,0 +1,13 @@
+import { FlexBox} from "../../styles/globals";
+import Menu from "../../components/Menu";
+
+export default function Login() {
+    return (
+        <div>
+            <FlexBox>
+                <Menu/>
+            </FlexBox>
+            logged in
+        </div>
+    )
+}
