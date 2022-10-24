e9cabee3379111c636d91a60201baa7800611c75
Monika Szucs <monika.szucs.work@gmail.com>
2022-10-24 16:27:32 -0700

    testing

--- Week-7/pokedex-api-server-side/pages/api/one.js
+++
@@ -1,7 +0,0 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-// http://localhost:3000/api/one
-export default function handlerOne(req, res) {
-  res.send('John Doe' )
-}
-
-
